36f9ade74a80f7f5f203102d9fac0ec245e50a84
mattdc <matt2021@outlook.com>
2024-01-07 13:51:10 +0000

    Use nlp library to check responses

--- analysis/output_analyzer.py
+++ analysis/output_analyzer.py
@@ -1,4 +1,9 @@
+import spacy
+
 class OutputAnalyzer:
+    def __init__(self):
+            self.nlp = spacy.load("en_core_web_sm")
+
     def analyze_output(self, outputs):
         analysis_summary = {
             "total": 0,
@@ -25,6 +30,24 @@ class OutputAnalyzer:
 
     def is_correct(self, response, expected_answer, answer_logic):
         if answer_logic == "Contains":
-            return expected_answer in response
+            doc_response = self.nlp(response)
+            doc_expected_answer = self.nlp(expected_answer)
+            
+            # Iterate over each token in the expected answer
+            for token1 in doc_expected_answer:
+                # Iterate over each token in the response
+                found_match = False
+                for token2 in doc_response:
+                    similarity_score = token1.similarity(token2)
+                    
+                    if similarity_score > 0.8:
+                        found_match = True
+                        break
+                
+                if not found_match:
+                    return False
+            
+            # If all tokens in the expected answer have a match, return True
+            return True
 
         return False
